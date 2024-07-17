d47b0e239898b77ea1c08d7894abcdd5a2b8d403
Алексей Зенков <zenkov.m700@mail.ru>
2024-07-17 19:35:48 +0300

    [GPL-5] Добавлено управление персонажем с клавиатуры

---
+++ Person.js
@@ -0,0 +1,36 @@
+class Person extends GameObject {
+    constructor(config) {
+        super(config);
+
+        this.movingProgrssRemaining = 0;
+
+        this.isPlayerControlled = config.isPlayerControlled || false
+
+        this.directionUpdate = {
+            "up": ["y", -1],
+            "down": ["y", 1],
+            "left": ["x", -1],
+            "right": ["x", 1],
+        }
+
+    }
+
+
+    update(state) {
+        this.updatePosition()
+
+        if(this.isPlayerControlled && this.movingProgrssRemaining === 0 && state.arrow){
+            this.direction = state.arrow;
+            this.movingProgrssRemaining = 16;
+        }
+    }
+
+    updatePosition() {
+        if (this.movingProgrssRemaining > 0) {
+            const [property, change] = this.directionUpdate[this.direction]
+            this[property] += change;
+            this.movingProgrssRemaining -= 1;
+        }
+
+    }
+}
