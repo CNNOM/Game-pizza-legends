6057fcdce02ea052584e0b88e86c15c5622a1137
Алексей Зенков <zenkov.m700@mail.ru>
2024-07-17 16:16:07 +0300

    [GPL-3] Переделан способ отображение персонажей. Добавлена возможность добавлять несколько персонажей.

--- Overworld.js
+++ Overworld.js
@@ -6,48 +6,28 @@ class Overworld {
     }
 
     init(){
-        console.log("Привет из другого мира", this)
 
+        // Игровое Поле
         const image = new Image();
         image.src = "/images/maps/DemoLower.png"
         image.onload = () => {
             this.ctx.drawImage(image, 0, 0);
         }
 
-        const x = 5; //начальные координаты персонажи и его тени
-        const y = 6; //начальные координаты персонажи и его тени
+        //Размешение Несколько Игровых Объектов
+        const hero = new GameObject({
+            x: 5,
+            y: 6,
+        })
+        const npc1 = new GameObject({
+            x: 7,
+            y: 9,
+            src: "/images/characters/people/npc1.png"
+        })
 
-        const hero = new Image();
-        hero.src = "/images/characters/people/hero.png"
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero,
-                0, //точка left начала
-                0, //точка top начала
-                32, // ширина разреза
-                32, // высота разреза
-                x * 16 - 8, // точка нахождения пресонажа, умножаем на 16, так как размер ондой клетки 16px
-                y * 16 - 18, // точка нахождения пресонажа
-                32, // ширина персонажа
-                32, // высота персонажа
-            );
-        }
-
-        // создаём тень персонажаы
-        const shadow = new Image();
-        shadow.src = "/images/characters/shadow.png"
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow,
-                0,
-                0,
-                32,
-                32,
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32,
-            );
-        }
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 200)
     }
 }
